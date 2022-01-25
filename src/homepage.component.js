334ada7a943d0c9200a9177a8059c2539d35e5ea
Shivam Sharma <shahishiv99@gmail.com>
2022-01-25 15:28:25 +0530

    Add website

---
+++ src/homepage.component.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='tital'> HATS</h1>
+          <span className='subtital'> SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='tital'> JAKCETS</h1>
+          <span className='subtital'> SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='tital'> SNEAKERS</h1>
+          <span className='subtital'> SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='tital'> WOMENS</h1>
+          <span className='subtital'> SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='tital'> MENS</h1>
+          <span className='subtital'> SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default HomePage;
